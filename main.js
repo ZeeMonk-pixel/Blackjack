79d2a6fd7b999ba7b55d3d84a6b053224f462a03
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2022-09-27 12:54:08 +0100

    number guessing game

---
+++ main.js
@@ -0,0 +1,19 @@
+let firstCard = 5
+let secondCard = 6
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack!")
+} else {
+    console.log("you're out of the game!")
+}
+
+let age = 22
+
+if (age < 21){
+    console.log("You can not enter the club!")
+} else {
+    console.log("Welcome!")
+}
